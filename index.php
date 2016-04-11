795c5d4a8a47601ed9813b9d9bece44fd936a2fb
chin3816 <yarely2011@yahoo.com>
2016-04-11 21:55:52 +0000

    User can filter data using three fields and sort the results in asc and desc.

--- index.php
+++ index.php
@@ -1,8 +1,133 @@
 <?php
-     session_start();
+    session_start();
     include('includes/database.php');
     $dbConn = getDatabaseConnection('ooa');
+    
+function getContributors(){
+    //this method gets all  contributos from the database
+     global $dbConn;
+     
+     $sql = "SELECT contributorName FROM contributor";
+       //prepare, execute, fetch
+    $statement =  $dbConn->prepare($sql);
+    $statement->execute();
+    $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+   
+    return $records;  
+    
+}
+  
 
+function getRegion(){
+    //this method gets all  contributos from the database
+     global $dbConn;
+     
+     $sql = "SELECT region FROM product GROUP BY region";
+       //prepare, execute, fetch
+    $statement =  $dbConn->prepare($sql);
+    $statement->execute();
+    $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+   print_r($records);
+    return $records;  
+    
+}
+    
+function getProducts() {
+    global $dbConn;
+    
+    $sql = "SELECT productId, productName, price, era, region, productType
+    FROM product NATURAL JOIN productType NATURAL JOIN contributor";
+
+    if (isset($_GET['inputForm'])) { //checks if the fomr is submitted
+
+        //this  filters the data by era
+        if (isset($_GET['filertByEra'])) {
+      
+            $era = "none";
+            if($_GET['filertByEra'] == "1750"){
+                $era = " WHERE era > 1750 and era <= 1850";
+            }else if($_GET['filertByEra'] == "1850"){
+                $era = " WHERE era > 1850 and era <= 1950";
+            }else if($_GET['filertByEra'] == "1950"){
+                $era = " WHERE era > 1950";
+            }else{
+                $era = "none"; //in the case that all is selected
+            }
+        }
+        
+          //this  filters the data by type
+        if (isset($_GET['filertByType']) && $_GET['filertByType'] != "all") {
+      
+            if($era = "none"){
+                $era = " WHERE";//if there is no filter by era we add the WHERE clause
+            }else{
+                $era .= " AND"; //If there is other filters included
+            }
+        
+            $era .= " productType = :productType"; //Using Named Parameters to prevent SQL Injection
+            $namedParameters[":productType"] =  $_GET['filertByType']; //sending parameters
+        }
+        
+          //this  filters the data by type
+        if (isset($_GET['contributorName']) && $_GET['contributorName'] != "all") {
+      
+            if($era != "none"){
+                 $era .= " AND"; //If there is other filters included 
+            }else{
+                 $era = " WHERE";//if there is no filter added  we add the WHERE clause
+            }
+        
+            $era .= " contributorName = :contributorName"; //Using Named Parameters to prevent SQL Injection
+            $namedParameters[":contributorName"] =  $_GET['contributorName']; //sending parameters
+
+        }
+    
+           //this  filters the data by type
+        if (isset($_GET['region']) && $_GET['region'] != "all") {
+      
+            if($era != "none"){
+                 $era .= " AND"; //If there is other filters included 
+            }else{
+                 $era = " WHERE";//if there is no filter added  we add the WHERE clause
+            }
+        
+            $era .= " region = :region"; //Using Named Parameters to prevent SQL Injection
+            $namedParameters[":region"] =  $_GET['region']; //sending param
+        }
+        if($era != "none"){
+            $sql .= $era;
+        }
+         
+        
+        if (isset($_GET['orderBy'])) {
+      
+            if($_GET['orderBy'] == "priceLow"){
+                $sql .= " ORDER BY price ASC";
+            }else if($_GET['orderBy'] == "priceHigh"){
+                 $sql .= " ORDER BY price DESC";
+            }else if($_GET['orderBy'] == "nameAsc"){
+                $sql .= " ORDER BY productName ASC";
+            }else{
+                $sql .= " ORDER BY productName DESC";
+            }
+        }
+        
+        
+         //prepare, execute, fetch
+    $statement = $dbConn->prepare($sql);
+    $statement->execute($namedParameters);
+    $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    return $records; 
+
+    }  
+    
+    return (array());
+    
+    
+}
 
 ?>
 <!DOCTYPE html>
@@ -11,6 +136,105 @@
         <title>Project 2</title>
     </head>
     <body>
-
+        <h1> Ott's Oddities and ...</h1>
+        <main>
+             <?php
+            
+             ?>
+            <form>
+                <strong>Sort by: </strong>
+                  <select name="orderBy">
+                      <option value="priceLow"> Price: Low to High </option>
+                      <option value="priceHigh"> Price: High to Low </option>
+                      <option value="nameAsc"> Name: Ascending Order </option>
+                      <option value="nameDes"> Name: Descending Order </option>
+                  </select>
+                  <br/>
+                  
+                  <h3>Filter By </h3>
+                 
+                  <strong>Era:   </strong>
+                   <input type="radio" name="filertByEra" value="all" id = "one" checked><label for="one">All</label>
+                  <input type="radio" name="filertByEra" value="1750" id = "two"><label for="two">1750's - 1850's</label> 
+                  <input type="radio" name="filertByEra" value="1850" id = "three"><label for="three">1850's - 1950's</label> 
+                  <input type="radio" name="filertByEra" value="1950" id = "four"><label for="four">1950's - 2000's</label>
+                 
+            
+                 <br/>
+                  <strong>Type   </strong>
+                   <input type="radio" name="filertByType" value="all" id = "oneType" checked><label for="oneType"> All</label>
+                  <input type="radio" name="filertByType" value="Cloth" id = "twoType"><label for="twoType"> Cloth</label> 
+                  <input type="radio" name="filertByType" value="Furniture" id = "threeType"><label for="threeType"> Furniture</label> 
+                     <input type="radio" name="filertByType" value="Ceramics" id = "fourType"><label for="fourType"> Ceramics</label> 
+                  <input type="radio" name="filertByType" value="Jewelry" id = "fiveType"><label for="fiveType">Jewelry</label>
+                  <input type="radio" name="filertByType" value="Metal" id = "sixType"><label for="sixType">Metal</label>
+                  
+                  <br/>
+                  
+                  <!--This first get the contributors names from the database to then display them-->
+                  <strong>Contributor Name: </strong>
+                   <select name="contributorName"> <!--the data will later be retrive using the contributorName-->
+                      <option value="all"> All </option>
+                          <?php
+                             $contributorNames = getContributors(); //retrives the names 
+                             foreach ($contributorNames as $candidate){
+                           
+                               echo "<option value='".$candidate['contributorName']."'>" . $candidate['contributorName'] . " </option>";  
+                   
+                            }
+                        ?>
+                  </select>
+                  
+                     <strong>Region: </strong>
+                    <select name="region">
+                      <option value="all"> All </option>
+                          <?php
+                             $regionsAvailable = getRegion();
+                             foreach ($regionsAvailable as $region){
+                               echo "<option value='".$region['region']."'>" . $region['region'] . " </option>";  
+                   
+                            }
+                        ?>
+                  </select>
+                 
+                 <br/>
+                  <br/>
+                  <input type="submit" value="Search Products" name="inputForm">
+                  
+            </form>
+            
+            <!--Table to display output-->
+         <table border = "1">
+        <!--New table row-->
+        <tr>
+             <th> Name </th>
+             <th> Price</th>
+              <th> Era</th>
+             <th> Region </th>
+             <th> Product Type</th>
+        </tr>
+             <?php
+             
+             $records = getProducts();
+             foreach ($records as $record) {
+                 echo "<tr>";
+                     echo "<td>";
+                        echo $record['productName'];
+                     echo "</td>";
+                     
+                     echo "<td>".$record['price']."</td>"; //displays the price, era, region, and product Type
+                     echo "<td>".$record['era']."</td>";
+                     echo "<td>".$record['region']."</td>";
+                     echo "<td>".$record['productType']."</td>";
+                 
+                 echo "</tr>";
+             }
+             ?>
+          </table>
+        
+         
+         
+          
+        </main>
     </body>
 </html>
